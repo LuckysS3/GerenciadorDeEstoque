241d72acbb26fc2f427d3559e73a9c414b4a2d44
Luiz André <122027228+LuckysS3@users.noreply.github.com>
2025-05-02 11:10:54 -0300

    m

--- Gerenciador De Estoque/Utils/StockContext.cs
+++ Gerenciador De Estoque/Utils/StockContext.cs
@@ -1,6 +1,16 @@
-﻿namespace Gerenciador_De_Estoque.Utils;
+﻿using Gerenciamento_De_Estoque.Models;
+using Microsoft.EntityFrameworkCore;
 
-internal class StockContext
+namespace Gerenciador_De_Estoque.Utils;
+
+internal class StockContext : DbContext
 {
+    public DbSet<ProductModel> Product { get; set; }
 
+    private readonly string connectionString = "server=localhost;database=GerenciadorDeEstoque;user=root;password=luiz";
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+    }
 }
